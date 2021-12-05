21baabe1bece143becc413ec85e56fab14e25405
Jingyi Huang <jingyih1999@gmail.com>
2021-12-05 00:01:29 -0500

    modify remove() method

--- src/main/java/com/manage/dao/NodesCenter.java
+++ src/main/java/com/manage/dao/NodesCenter.java
@@ -3,6 +3,9 @@ package com.manage.dao;
 import com.manage.controller.WorkNode;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -14,15 +17,15 @@ public class NodesCenter {
         return workNodeQueue;
     }
 
-    public void add(WorkNode workNode) {
-        try {
-            workNodeQueue.put(workNode);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void add(WorkNode workNode) throws InterruptedException {
+        workNodeQueue.put(workNode);
     }
 
-    public WorkNode get() {
+    public boolean remove(WorkNode workNode) {
+        return workNodeQueue.remove(workNode);
+    }
+
+    public WorkNode getHead() {
         // dequeue head and return workNode
         WorkNode workNode = null;
         try {
@@ -31,6 +34,16 @@ public class NodesCenter {
             e.printStackTrace();
         }
         return workNode;
+    }
+
+    public List<WorkNode> traverse() {
+        // traverse BlockingQueue and return a list of nodes
+        List<WorkNode> queue2List = new ArrayList<>();
+        Iterator<WorkNode> iterator = workNodeQueue.iterator();
+        while (iterator.hasNext()) {
+            queue2List.add(iterator.next());
+        }
+        return queue2List;
     }
 
 }
