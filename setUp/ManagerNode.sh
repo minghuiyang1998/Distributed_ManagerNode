91041bbf6b95dd0c07f5e3fb592c284cb095ae49
yuhe peng <y.peng1998@gmail.com>
2021-12-08 14:10:56 -0500

    modify set up script

--- setUp/ManagerNode.sh
+++ setUp/ManagerNode.sh
@@ -1,5 +1,4 @@
 sudo apt update
-sudo apt install default-jre
-sudo apt install default-jdk
+sudo apt install openjdk-8-jdk
 sudo wget https://github.com/minghuiyang1998/PAFinal_MNode/blob/install/target/PAFinal_MNode-0.0.1-SNAPSHOT.jar
 sudo java PAFinal_MNode-0.0.1-SNAPSHOT.jar
--- setUp/WorkerNode.sh
+++ setUp/WorkerNode.sh
@@ -1,6 +1,7 @@
 sudo apt update
-sudo apt install default-jre
-sudo apt install default-jdk
+sudo apt install openjdk-8-jdk
+sudo rm Worker.java
+sudo rm Worker.class
 sudo wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/JessiePen/PAFinal_WNode/yuhe/src/Worker.java
-sudo javac Woker.java
-sudo java Worker.java
+sudo javac Worker.java
+sudo java Worker
