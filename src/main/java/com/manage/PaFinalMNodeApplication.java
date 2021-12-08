402c2414ccb8908761ac3398bd06ab062c13a8a2
Jingyi Huang <jingyih1999@gmail.com>
2021-12-07 22:33:25 -0500

    exp: log writer initiation

--- src/main/java/com/manage/PaFinalMNodeApplication.java
+++ src/main/java/com/manage/PaFinalMNodeApplication.java
@@ -1,11 +1,13 @@
 package com.manage;
 
+import com.manage.dao.LogWriter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaFinalMNodeApplication {
     public static void main(String[] args) {
+        LogWriter.initLogFile();
         SpringApplication.run(PaFinalMNodeApplication.class, args);
     }
 }
